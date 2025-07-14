da464e4c1c632b80aa8b5e388452c9314c008231
Badreddine Khalil <badreddinekhalil@example.com>
2025-07-14 19:06:53 +0200

    version final docker-compose.yml (order, rabbit , mysql , product )

--- product-service/wait-for.sh
+++ product-service/wait-for.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
-
-host="$1"
-port="$2"
-shift 2
-cmd="$@"
-
-until nc -z "$host" "$port"; do
-  echo "MySQL is unavailable - sleeping"
-  sleep 2
-done
-
-echo "MySQL is up - executing command"
-exec $cmd
+host="$1"; port="$2"; shift 2
+echo "⏳ Attente de $host:$port..."
+until nc -z "$host" "$port"; do sleep 2; done
+echo "✅ $host:$port prêt, on démarre !"
+exec "$@"
