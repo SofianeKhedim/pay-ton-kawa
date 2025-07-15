bb521cd1167979e319855dddc64c64213110dcaf
Zvki1 <rzaki@hotmail.fr>
2025-07-15 13:58:04 +0200

    add rabitmq product consumation

--- order-service/src/utils/rabbitmq.js
+++ order-service/src/utils/rabbitmq.js
@@ -1,20 +1,45 @@
-const amqp = require('amqplib');
-
+const amqp = require("amqplib");
+const Order = require("../models/order.model");
+const { v4: uuidv4 } = require('uuid');
 let channel;
 
 async function connectRabbitMQ(retries = 5, delay = 3000) {
   while (retries > 0) {
     try {
-      const connection = await amqp.connect('amqp://rabbitmq:5672');
+      const connection = await amqp.connect("amqp://localhost:5672");
       channel = await connection.createChannel();
-      await channel.assertQueue('order_events');
-      console.log('✅ Connected to RabbitMQ');
+      await channel.assertQueue("order_events");
+      await channel.assertQueue("stock_events");
+      console.log("✅ Connected to RabbitMQ");
+      channel.consume("stock_events", async (msg) => {
+        if (msg !== null) {
+          const content = JSON.parse(msg.content.toString());
+          console.log("📥 Réponse stock reçue :", content);
+
+          const { orderId } = content.data;
+          const event = content.event;
+
+          let newStatus = "pending";
+          if (event === "stock_validated") newStatus = "validated";
+          if (event === "stock_failed") newStatus = "failed";
+
+          await Order.findByIdAndUpdate(orderId, { orderStatus: newStatus });
+          console.log(
+            `🔄 Statut commande ${orderId} mis à jour : ${newStatus}`
+          );
+
+          channel.ack(msg); 
+        }
+      });
+
       break;
     } catch (err) {
       console.error(`❌ RabbitMQ connection error: ${err.message}`);
       retries--;
       if (retries === 0) {
-        console.error('💥 Failed to connect to RabbitMQ after several attempts');
+        console.error(
+          "💥 Failed to connect to RabbitMQ after several attempts"
+        );
         process.exit(1);
       }
       console.log(`🔁 Retrying in ${delay / 1000}s... (${retries} tries left)`);
@@ -23,26 +48,24 @@ async function connectRabbitMQ(retries = 5, delay = 3000) {
   }
 }
 
-
 function publishOrderEvent(order) {
-  if (!channel) throw new Error('RabbitMQ channel not initialized');
+  if (!channel) throw new Error("RabbitMQ channel not initialized");
 
   const message = {
-    event: 'order_created',
+    event: "order_created",
     data: {
       orderId: order._id,
       clientId: order.clientId,
-      products: order.products,  
+      products: order.products,
       orderStatus: order.orderStatus,
       createdAt: order.createdAt,
-    }
+    },
   };
 
-  channel.sendToQueue('order_events', Buffer.from(JSON.stringify(message)));
+  channel.sendToQueue("order_events", Buffer.from(JSON.stringify(message)));
 }
-
 
 module.exports = {
   connectRabbitMQ,
-  publishOrderEvent
+  publishOrderEvent,
 };
