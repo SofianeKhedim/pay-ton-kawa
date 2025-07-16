5d8db2aacaab50745a1bd29d0c3aefe033ac3f2e
SofianeKhedim <sofianeinfo2003@gmail.com>
2025-07-17 01:55:23 +0200

    juste to test CI/CD

--- product-service/src/main/java/com/example/mspr4/Mspr4Application.java
+++ product-service/src/main/java/com/example/mspr4/Mspr4Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Mspr4Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Mspr4Application.class, args);
 	}
