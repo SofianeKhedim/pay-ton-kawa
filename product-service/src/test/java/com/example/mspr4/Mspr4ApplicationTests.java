0d302e8a8fbb0c669db8afb922b435548943ec5f
SofianeKhedim <sofianeinfo2003@gmail.com>
2025-07-17 01:50:43 +0200

    fix products tests

--- product-service/src/test/java/com/example/mspr4/Mspr4ApplicationTests.java
+++ product-service/src/test/java/com/example/mspr4/Mspr4ApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.mspr4;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Mspr4ApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assert true;
 	}
 }
